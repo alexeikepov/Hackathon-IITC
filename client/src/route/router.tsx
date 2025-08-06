85683c5d3544d5a0f3985c80a2986248210bdafa
alexeikepov <alexeikepov@gmail.com>
2025-08-07 00:08:22 +0300

    12312

--- client/src/route/router.tsx
+++ client/src/route/router.tsx
@@ -1,37 +1,36 @@
-import {  RootLayout } from "@/components/RootLayout";
+import { RootLayout } from "@/components/RootLayout";
 import { SchedulePage } from "@/components/SchedulePage";
 import { SyllabusPage } from "@/components/SyllabusPage";
 import { CourseDetailsPage } from "@/pages/CourseDetailsPage";
 import { CoursesPage } from "@/pages/CoursesPage";
 import { CreateCoursePage } from "@/pages/CreateCoursePage";
-import { HomePage } from "@/pages/HomePage";
+import { DashboardPage } from "@/pages/DashboardPage";
 import { ProtectRoutes } from "./protectedRoutes";
 
-import LoginPage from "@/pages/auth-page";
+import LoginPage from "@/pages/login-page";
 import RegistrationPage from "@/pages/register-page";
 import { createBrowserRouter } from "react-router-dom";
 
-
 export const Router = createBrowserRouter([
-    { path: "/login", element: <LoginPage /> },
-    { path: "/register", element: <RegistrationPage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/register", element: <RegistrationPage /> },
 
-    {
+  {
+    path: "/",
+    element: <ProtectRoutes />,
+    children: [
+      {
         path: "/",
-        element: <ProtectRoutes />,
+        element: <RootLayout />,
         children: [
-            {
-                path: "/",
-                element: <RootLayout />,
-                children: [
-                    { index: true, element: <HomePage /> },
-                    { path: "/syllabus", element: <SyllabusPage /> },
-                    { path: "/schedule-page", element: <SchedulePage /> },
-                    { path: "create-course", element: <CreateCoursePage /> },
-                    { path: "/courses", element: <CoursesPage /> },
-                    { path: "/courses/:id", element: <CourseDetailsPage /> }
-                ],
-            },
+          { index: true, element: <DashboardPage /> },
+          { path: "/syllabus", element: <SyllabusPage /> },
+          { path: "/schedule-page", element: <SchedulePage /> },
+          { path: "create-course", element: <CreateCoursePage /> },
+          { path: "/courses", element: <CoursesPage /> },
+          { path: "/courses/:id", element: <CourseDetailsPage /> },
         ],
-    },
+      },
+    ],
+  },
 ]);
