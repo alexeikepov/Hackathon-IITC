fb701586950d51c0e9adfa2db9df4cc9349ec26d
alexeikepov <alexeikepov@gmail.com>
2025-08-07 06:47:24 +0300

    123

--- client/src/services/course.service.ts
+++ client/src/services/course.service.ts
@@ -9,3 +9,19 @@ export async function getAllCourses() {
 
   return res.json();
 }
+
+export async function unassignCourse(courseId: string) {
+  const res = await fetch(
+    `http://localhost:3001/api/courses/${courseId}/unassign`,
+    {
+      method: "PATCH",
+      credentials: "include",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to unassign course");
+  }
+
+  return res.json();
+}
